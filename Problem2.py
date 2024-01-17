2ecb9dd771c9811ee57d575b9318130ae47b7f55
sanu2a <ailasana88@gmail.com>
2024-01-17 11:30:30 +0100

    pb2,3

---
+++ Problem2.py
@@ -0,0 +1,99 @@
+## Problem 1  : Chained Roseenbrock Function
+import numpy as np 
+from method1 import *
+from method2 import *
+import matplotlib.pyplot as plt
+import scipy.sparse as sp
+def fk(x, k):
+    if k % 2 == 1:
+        return 10 * (x[k-1]**2 - x[k])
+    else:
+        return x[k-2] - 1
+
+def F(x):
+    k_values = np.arange(1, len(x) + 1)
+    terms = np.vectorize(lambda k: fk(x, k))(k_values)
+    return 0.5 * np.sum(terms**2)
+
+
+def gradf(x):
+    xm = x[1:-1]
+    xm_m1 = x[:-2]
+    xm_p1 = x[2:]
+    der = np.zeros_like(x)
+    der[1:-1] = 200*(xm-xm_m1**2) - 400*(xm_p1 - xm**2)*xm - 2*(1-xm)
+    der[0] = -400*x[0]*(x[1]-x[0]**2) - 2*(1-x[0])
+    der[-1] = 200*(x[-1]-x[-2]**2)
+    return der
+
+
+def hessf(x):
+    H = np.diag(-400*x[:-1],1) - np.diag(400*x[:-1],-1)
+    diagonal = np.zeros_like(x)
+    diagonal[0] = 1200*x[0]**2-400*x[1]+2
+    diagonal[-1] = 200
+    diagonal[1:-1] = 202 + 1200*x[1:-1]**2 - 400*x[2:]
+    H = H + np.diag(diagonal)
+    return H
+
+
+
+def run_test(f, gradf, hessf, x0, kmax, tolgrad, btmax, c1, rho):
+    print("========== Test Results Modifed newton ==========")
+    # print("Starting point:", x0)
+    from timeit import default_timer as timer
+    start = timer()
+    k, x_seq, f_vals, grad_norm_seq ,btseq = modified_newton(f, gradf, hessf, x0, kmax,tolgrad, rho, c1, btmax)
+    end = timer()
+    print(end - start)
+    #("Final solution xk =", x_seq[-1])
+    print("Minimum function value =", f_vals[-1])
+    print("Number of iterations =", k)
+    print(grad_norm_seq[-1])
+    plt.semilogy(range(k), f_vals)
+    plt.show()
+    plt.semilogy(range(k), grad_norm_seq)
+    plt.show()
+    plt.plot(range(k)[1:], btseq)
+    plt.show()
+
+
+def run_test_fd(f, x0, kmax, fd, tolgrad):
+    print("========== Test Results ==========")
+    # print("Starting point:", x0)
+    from timeit import default_timer as timer
+    start = timer()
+    k, x_seq, f_vals, grad_norm_seq ,btseq = modified_newton_FD(f,x0,kmax,fd,tolgrad,rho,c1,btmax)  
+    end = timer()
+    print("time" , end - start)
+    # print(grad_norm_seq)
+    #print("Final solution xk =", x_seq[-1])
+    print("Minimum function value =", f_vals[-1])
+    print("Number of iterations =", k)
+    print(grad_norm_seq[-1])
+    plt.plot(range(k), f_vals)
+    plt.show()
+    plt.plot(range(k), grad_norm_seq)
+    plt.show()
+    plt.plot(range(k)[1:], btseq)
+    plt.show()
+
+
+
+
+if __name__ == '__main__':
+
+    np.random.seed(42)  
+    kmax = 1000 
+    dict_val = {1 : -1.2, 0: 1}
+    tolgrad = 1e-6
+    btmax = 100
+    rho = 0.4 ## 0.3
+    c1 = 1e-4
+    n = 10**2
+
+    x0 = np.array([dict_val[i%2] for i in range(n)])
+    ## Method 2
+    run_test_fd(F,x0,kmax,"C",tolgrad)
+    ## Method 1
+    #run_test(F, gradf, hessf, x0, kmax, tolgrad,btmax, c1, rho)
--- Problem3.py
+++ Problem3.py
@@ -1,80 +1,88 @@
-## Problems : 25 
-
-## Problem 2 (25) : Extended 
-
-from math import exp
+## Problem 3  : Chained Roseenbrock Function
 import numpy as np 
 from method1 import *
 from method2 import *
+import matplotlib.pyplot as plt
+import scipy.sparse as sp
+def fk(x, k):
+    if k != 1:
+        return 10*(k-1)*(x[k-1] - x[k-2])**2
+    else:
+        return x[k-2]
 
+def F(x):
+    k_values = np.arange(1, len(x) + 1)
+    terms = np.vectorize(lambda k: fk(x, k))(k_values)
+    return 0.5 * np.sum(terms**2)
 
-def F(x): 
-    n = np.shape(x)[0]
-    Fx = 0
-    for i in range(1, n):
-        Fx += (x[i-1]**2)**(x[i]**2 + 1) + (x[i]**2)**(x[i-1]**2 + 1)
-    return Fx
-
-
-def F1(x):
-    n = np.shape(x)[0]
-    Fx = 0
-    k = n/2
-    for j in range(1, k):
-        i = 2*j
-        Fx += (x[i-1]**2)**(x[i]**2 + 1) + (x[i]**2)**(x[i-1]**2 + 1)
-    return Fx 
 
 def gradf(x):
-    n = np.shape(x)[0]
-    dx = sp.symbols(' '.join([f'x{i}' for i in range(n)]))
-    grad_F = [sp.diff(F(dx), var) for var in dx]
-    grad_F_func = sp.lambdify(dx, grad_F, 'numpy')
-    return np.array(grad_F_func(*x))
+    ##  TO BE IMPLEMENTED
+    return 
+
 
 def hessf(x):
-    n = len(x)
-    dx = sp.symbols(' '.join([f'x{i}' for i in range(n)]))
-    hessian_F = sp.hessian(F(dx), dx)
-    hessian_F_func = sp.lambdify(dx, hessian_F, 'numpy')
-    return hessian_F_func(*x)
-    
-def run_test_fd(method, f, x0, kmax, fd, tolgrad):
-    dictio = {"c" : "Centred", "FW" : "Forward"} 
-    print(f"========== Test Results {dictio[fd]} ==========")
-    print("Method:", method.__name__)
-    xk, fk, k, x_seq, f_vals = method(f, x0, kmax, fd, tolgrad)
-    print("Final solution xk =", xk)
-    print("MSE of the solution !", np.linalg.norm(xk))
-    print("Minimum function value =", round(fk, 3))
-    print("Number of iterations =", k)
+    ## TO BE IMPLEMENTED
+    return 
 
-def run_test(method, f, gradf, hessf, x0, kmax, tolgrad=1e-5):
-    print("========== Test Results ==========")
-    print("Method:", method.__name__)
-    print("Starting point:", x0)
-    xk, fk, k, x_seq, f_vals = method(f, gradf, hessf, x0, kmax, tolgrad)
-    print("Final solution xk =", xk)
-    print("MSE of the solution !", np.linalg.norm(xk))
-    print("Minimum function value =", round(fk, 3))
+
+
+def run_test(f, gradf, hessf, x0, kmax, tolgrad, btmax, c1, rho):
+    print("========== Test Results Modifed newton ==========")
+    # print("Starting point:", x0)
+    from timeit import default_timer as timer
+    start = timer()
+    k, x_seq, f_vals, grad_norm_seq ,btseq = modified_newton(f, gradf, hessf, x0, kmax,tolgrad, rho, c1, btmax)
+    end = timer()
+    print(end - start)
+    #("Final solution xk =", x_seq[-1])
+    print("Minimum function value =", f_vals[-1])
     print("Number of iterations =", k)
-    
+    print(grad_norm_seq[-1])
+    plt.semilogy(range(k), f_vals)
+    plt.show()
+    plt.semilogy(range(k), grad_norm_seq)
+    plt.show()
+    plt.plot(range(k)[1:], btseq)
+    plt.show()
+
+
+def run_test_fd(f, x0, kmax, fd, tolgrad):
+    print("========== Test Results ==========")
+    # print("Starting point:", x0)
+    from timeit import default_timer as timer
+    start = timer()
+    k, x_seq, f_vals, grad_norm_seq ,btseq = modified_newton_FD(f,x0,kmax,fd,tolgrad,rho,c1,btmax)  
+    end = timer()
+    print("time" , end - start)
+    # print(grad_norm_seq)
+    #print("Final solution xk =", x_seq[-1])
+    print("Minimum function value =", f_vals[-1])
+    print("Number of iterations =", k)
+    print(grad_norm_seq[-1])
+    plt.plot(range(k), f_vals)
+    plt.show()
+    plt.plot(range(k), grad_norm_seq)
+    plt.show()
+    plt.plot(range(k)[1:], btseq)
+    plt.show()
+
+
+
+
 if __name__ == '__main__':
     np.random.seed(42)  
-    kmax = 1000
-    tolgrad = 1e-7
-    dict_val = {1: -1, 0: 1}
-
-    n = 10 * 3
-    x0 = np.array([dict_val[i % 2] for i in range(n)])
-    print(F1(x0))
-    print(F(x0))
+    kmax = 1000 
+    tolgrad = 1e-6
+    btmax = 100
+    rho = 0.4 ## 0.3
+    c1 = 1e-4
+    n = 10**3
+    # Starting point
+    x0 = np.array([-1.2 for i in range(n-1)])
+    x0 = np.append(x0, 1)
     
-    # # Test modified_newton method
-    # run_test(modified_newton, F, gradf, hessf, x0, kmax, tolgrad)
-
-    # # Test modified_newton_FD method with Forward Finite Differences
-    # run_test_fd(modified_newton_FD, F, x0, kmax, "FW", tolgrad)
-
-    # # Test modified_newton_FD method with Centred Finite Differences
-    # run_test_fd(modified_newton_FD, F, x0, kmax, "c", tolgrad)
+    ## Method 1
+    run_test_fd(F,x0,kmax,"C",tolgrad)
+    ## Method 2 
+    #run_test(F, gradf, hessf, x0, kmax, tolgrad,btmax, c1, rho)
